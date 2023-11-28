2d0a48f28289f30b061c2d95ff155116ab78c223
6uiwj <oterges00@gmail.com>
2023-11-29 01:31:50 +0900

    commit.2nd(231129) 페이징1 - criteria생성, 페이지당 보여줄 게시글 출력

--- login2/src/main/java/com/login/common/boardListController.java
+++ login2/src/main/java/com/login/common/boardListController.java
@@ -1,6 +1,8 @@
 package com.login.common;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.momo.dao.BoardDao;
+import com.momo.dao.BoardDaoOrder;
+import com.momo.dto.BoardDto;
+import com.momo.dto.Criteria;
 
 /**
  * Servlet implementation class boardListController
@@ -18,8 +23,12 @@ public class boardListController extends HttpServlet {
        
    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		BoardDao dao = new BoardDao();
-		request.setAttribute("list", dao.getList());
+		Criteria cri = new Criteria(request.getParameter("pageNo"), request.getParameter("amount"));
+		BoardDaoOrder dao = new BoardDaoOrder();
+		
+		List<BoardDto> list = dao.getList(cri);		
+		request.setAttribute("list", list);
+		System.out.println("boardListController - dao list생성성공");
 		dao.close();
 		//response.sendRedirect("/login/board.jsp");
 		request.getRequestDispatcher("/login/board.jsp").forward(request, response);
---
+++ login2/src/main/java/com/momo/dto/Criteria.java
@@ -0,0 +1,66 @@
+package com.momo.dto;
+
+public class Criteria {
+	/**
+	 * 리스트를 조회하기 위한 파라미터 세팅
+	 */
+	private int pageNo=1;
+	private int amount = 10;
+	private int endNo;
+	private int startNo;
+	
+	/**
+	 * 생성자를 통해 페이지번호와 페이지당 게시물의 수를 받아와서
+	 * 시작번호와 끝번호를 구합니다.
+	 * @param pageNo
+	 * @param amount
+	 */
+	public Criteria(String pageNo, String amount) {
+		
+	if(pageNo!=null && !"".equals(pageNo)) {
+		this.pageNo=Integer.parseInt(pageNo);
+	}
+	if(amount!=null && !"".equals(amount)) {
+		this.amount=Integer.parseInt(amount);
+	}
+	System.out.println("Criteria - pageNo : " + pageNo);
+	System.out.println("Criteria - amount : " + amount);
+	
+	endNo = this.pageNo*this.amount;
+	startNo = endNo - (this.amount - 1);
+	System.out.println("Criteria - endNo : " + endNo);
+	System.out.println("Criteria - startNo : " + startNo);
+	}
+
+	public int getPageNo() {
+		return pageNo;
+	}
+
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public int getEndNo() {
+		return endNo;
+	}
+
+	public void setEndNo(int endNo) {
+		this.endNo = endNo;
+	}
+
+	public int getStartNo() {
+		return startNo;
+	}
+
+	public void setStartNo(int startNo) {
+		this.startNo = startNo;
+	}
+}
